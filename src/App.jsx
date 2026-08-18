6315edb256a4a5921d76e7db29a59cb996c8c567
shunyavkash-jay-kalathiya <jay.kalathiya@shunyavkash.com>
2026-08-18 18:30:48 +0530

    add alll basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,62 @@
-import { useState } from 'react'
-import { Canvas, useThree } from '@react-three/fiber'
-import Dog from './components/Dog'
-import './App.css'
+import { Canvas } from "@react-three/fiber";
+import Dog from "./components/Dog";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import "./";
+
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 function App() {
+  // useGSAP(() => {
+  //   const tl = gsap.timeline({
+  //     scrollTrigger: {
+  //       trigger: "#sec-1",
+  //       start: "top top",
+  //       markers: true,
+  //       scrub: true,
+  //       endTrigger: "#sec-4",
+  //       end: "top top",
+  //     },
+  //   });
+
+  //   tl.to("html", {
+  //     backgroundColor: "#995555",
+  //   }).to("html", {
+  //     backgroundColor: "#559955",
+  //   });
+  // }, []);
 
   return (
     <>
-      <Canvas>
-        <Dog/>
-      </Canvas>
+      <main>
+        <Canvas style={{
+          height: "100vh",
+          width: "100vw",
+          position: "fixed",
+             top: 0,
+            left: 0,
+        }}>
+          <Dog />
+        </Canvas>
+        <section className="sec" id="sec-1">
+          section 1
+        </section>
+
+        <section className="sec" id="sec-2">
+          section 2
+        </section>
+
+        <section className="sec" id="sec-3">
+          section 3
+        </section>
+
+        <section className="sec" id="sec-4">
+          section 4
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,31 +1,66 @@
 import React from "react";
+import * as THREE from "three";
 import { useThree } from "@react-three/fiber";
-import { OrbitControls, useGLTF, useTexture } from "@react-three/drei";
-import { log } from "three";
-import { normalMap } from "three/tsl";
+import { OrbitControls, useAnimations, useGLTF, useTexture } from "@react-three/drei";
+import { color, normalMap } from "three/tsl";
+import { useEffect } from "react";
 
 function Dog() {
-  const model = useGLTF("/models/v20.glb");
+  const model = useGLTF("/models/dog.drc.glb");
 
   useThree(({ camera, scene, gl }) => {
-    console.log(camera.position);
-    camera.position.z = 2;
+    // console.log(camera.position);
+    camera.position.z = 0.55;
+    gl.toneMapping = THREE.ReinhardToneMapping;
+    gl.outputColorSpace = THREE.SRGBColorSpace;
   });
 
-  const texture = useTexture({
-    normalMap:"/models/waves.jpg"
-  })
+  // const texture = useTexture({
+  //   normalMap: "/dog_normals.jpg",
+  //   sampleMatcap:"/matcap/mat-2.png"
+  // });
+
+  // texture.normalMap.flipY = false
+  // texture.sampleMatcap.colorSpace = THREE.SRGBColorSpace
+
+  const { actions } = useAnimations(model.animations, model.scene);
+
+  useEffect(() => {
+    actions["Take 001"].play();
+  }, [actions]);
+
+  const [normalMap, sampleMatcap] = useTexture(["/dog_normals.jpg", "/matcap/mat-2.png"]).map((texture) => {
+    texture.flipY = false;
+    texture.colorSpace = THREE.SRGBColorSpace;
+    return texture;
+  });
+
+  const [branchMap, branchNormalMap] = useTexture(["/branches_diffuse.jpeg", "/branches_normals.jpeg"]).map((texture) => {
+    texture.colorSpace = THREE.SRGBColorSpace;
+    return texture;
+  });
+
+  const dogMaterial = new THREE.MeshMatcapMaterial({
+    normalMap: normalMap,
+    matcap: sampleMatcap,
+  });
+
+  const branchMaterial = new THREE.MeshMatcapMaterial({
+    normalMap: branchNormalMap,
+    map: branchMap,
+  });
 
   model.scene.traverse((child) => {
-
-    if (child.name.includes("wing")) {
-          console.log(child.name)
+    if (child.name.includes("DOG")) {
+      child.material = dogMaterial;
+    } else {
+      child.material = branchMaterial;
     }
-  })
+  });
 
   return (
     <>
-      <primitive object={model.scene} position={[0.3, -0, 0]} rotation={[0, Math.PI / 10  , 0]} />
+      <primitive object={model.scene} position={[0.25, -0.55, 0]} rotation={[0, Math.PI / 3.9, 0]} />
       <directionalLight position={[0, 5, 5]} color={0xffffff} intensity={10} />
       <OrbitControls />
     </>
