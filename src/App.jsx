d191f6ead51e1ed6aca5e469841a1331bd5c01d0
shunyavkash-jay-kalathiya <jay.kalathiya@shunyavkash.com>
2026-08-20 18:34:37 +0530

    add a trancison

--- src/App.jsx
+++ src/App.jsx
@@ -31,15 +31,15 @@ function App() {
     <>
       <main>
         <div className="images">
-          <img id='tommorowland' src="/tommorowland.png" alt="tommorowland"/>
-          <img id='navy-pier' src="/navy-pier.png" alt="navy-pier"/>
-          <img id='msi-chicago' src="/msi-chicago.png" alt="msi-chicago.png"/>
-          <img id='phone' src="/phone.png" alt="phone"/>
-          <img id='kikk' src="/kikk.png" alt="kikk"/>
-          <img id='kennedy' src="/kennedy.png" alt="kennedy"/>
-          <img id='opera' src="/opera.png" alt="opera"/>
+          <img id="tommorowland" src="/tommorowland.png" alt="tommorowland" />
+          <img id="navy-pier" src="/navy-pier.png" alt="navy-pier" />
+          <img id="msi-chicago" src="/msi-chicago.png" alt="msi-chicago.png" />
+          <img id="phone" src="/phone.png" alt="phone" />
+          <img id="kikk" src="/kikk.png" alt="kikk" />
+          <img id="kennedy" src="/kennedy.png" alt="kennedy" />
+          <img id="opera" src="/opera.png" alt="opera" />
         </div>
-        <Canvas id="canvas-item" >
+        <Canvas id="canvas-item">
           <Dog />
         </Canvas>
         <section className="sec" id="sec-1">
@@ -73,36 +73,52 @@ function App() {
                 <span className="small-title">2020 - ONGOING</span>
                 <h3 className="title-main">Tomorrowland</h3>
               </a>
-              <a img-title="navy-pier"  href="javascript:void(0)" className="title-item">
-                <span className="small-title">2020 - ONGOING</span>
-                <h3 className="title-main">Tomorrowland</h3>
+              <a img-title="navy-pier" href="javascript:void(0)" className="title-item">
+                <span className="small-title">2018 - Today</span>
+                <h3 className="title-main"> Navy Pier</h3>
               </a>
-              <a img-title="msi-chicago"  href="javascript:void(0)" className="title-item">
-                <span className="small-title">2020 - ONGOING</span>
-                <h3 className="title-main">Tomorrowland</h3>
+              <a img-title="msi-chicago" href="javascript:void(0)" className="title-item">
+                <span className="small-title">2015 - Today</span>
+                <h3 className="title-main"> MSI Chicago</h3>
               </a>
-              <a img-title="phone"  href="javascript:void(0)" className="title-item">
-                <span className="small-title">2020 - ONGOING</span>
-                <h3 className="title-main">Tomorrowland</h3>
+              <a img-title="phone" href="javascript:void(0)" className="title-item">
+                <span className="small-title">2016</span>
+                <h3 className="title-main"> This Was Louise’s Phone</h3>
               </a>
               <a img-title="kikk" href="javascript:void(0)" className="title-item">
-                <span className="small-title">2020 - ONGOING</span>
-                <h3 className="title-main">Tomorrowland</h3>
+                <span className="small-title">2012 - Today</span>
+                <h3 className="title-main"> KIKK Festival 2018</h3>
               </a>
               <a img-title="kennedy" href="javascript:void(0)" className="title-item">
-                <span className="small-title">2020 - ONGOING</span>
-                <h3 className="title-main">Tomorrowland</h3>
+                <span className="small-title">2017</span>
+                <h3 className="title-main"> The Kennedy Center</h3>
               </a>
               <a img-title="opera" href="javascript:void(0)" className="title-item">
-                <span className="small-title">2020 - ONGOING</span>
-                <h3 className="title-main">Tomorrowland</h3>
-              </a>            
+                <span className="small-title">2016 - Ongoing</span>
+                <h3 className="title-main"> Royal Opera Of Wallonia</h3>
+              </a>
             </div>
           </div>
         </section>
 
-        <section className="sec" id="sec-3">
-          section 3
+        <section id="sec-3">
+          <div className="container">
+            <div className="top">
+              <div className="left">
+                <h3>
+                  We're crafting <br /> emotional <br /> experiences aimed
+                  <br /> at improving <br /> results
+                </h3>
+              </div>
+              <div className="right"></div>
+            </div>
+            <div className="bottom">
+              <div className="left"></div>
+              <div className="right">
+                <p>Dogstudio is a design & technology firm working globally from our offices based in Belgium and Chicago. Our strong focus on producing high quality & emotional brandings, digital products and experiences became a signature.</p>
+              </div>
+            </div>
+          </div>
         </section>
       </main>
     </>
--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,3 +1,4 @@
+
 import React, { useEffect, useRef } from "react";
 import * as THREE from "three";
 import { useThree } from "@react-three/fiber";
@@ -42,15 +43,58 @@ function Dog() {
     return texture;
   });
 
+  const [mat1, mat2, mat3, mat4, mat5, mat6, mat7, mat8, mat9, mat10, mat11, mat12, mat13, mat14, mat15, mat16, mat17, mat18, mat19, mat20] = useTexture(["/matcap/mat-1.png", "/matcap/mat-2.png", "/matcap/mat-3.png", "/matcap/mat-4.png", "/matcap/mat-5.png", "/matcap/mat-6.png", "/matcap/mat-7.png", "/matcap/mat-8.png", "/matcap/mat-9.png", "/matcap/mat-10.png", "/matcap/mat-11.png", "/matcap/mat-12.png", "/matcap/mat-13.png", "/matcap/mat-14.png", "/matcap/mat-15.png", "/matcap/mat-16.png", "/matcap/mat-17.png", "/matcap/mat-18.png", "/matcap/mat-19.png", "/matcap/mat-20.png"]).map((texture) => {
+    texture.colorSpace = THREE.SRGBColorSpace;
+    return texture;
+  });
+
+  const material = useRef({
+    uMatcap1: { value: mat19 },
+    uMatcap2: { value: mat2 },
+    uProgress: { value: 1.0 },
+  });
+
   const dogMaterial = new THREE.MeshMatcapMaterial({
     normalMap: normalMap,
-    matcap: sampleMatcap,
+    matcap: mat2,
   });
 
   const branchMaterial = new THREE.MeshMatcapMaterial({
     normalMap: branchNormalMap,
     map: branchMap,
   });
+
+  function onBeforeCompile(shader) {
+    shader.uniforms.uMatcapTexture1 = material.current.uMatcap1;
+    shader.uniforms.uMatcapTexture2 = material.current.uMatcap2;
+    shader.uniforms.uProgress = material.current.uProgress;
+
+    // Store reference to shader uniforms for GSAP animation
+
+    shader.fragmentShader = shader.fragmentShader.replace(
+      "void main() {",
+      `
+        uniform sampler2D uMatcapTexture1;
+        uniform sampler2D uMatcapTexture2;
+        uniform float uProgress;
+
+        void main() {
+        `,
+    );
+
+    shader.fragmentShader = shader.fragmentShader.replace(
+      "vec4 matcapColor = texture2D( matcap, uv );",
+      `
+          vec4 matcapColor1 = texture2D( uMatcapTexture1, uv );
+          vec4 matcapColor2 = texture2D( uMatcapTexture2, uv );
+          float transitionFactor  = 0.2;
+          
+          float progress = smoothstep(uProgress - transitionFactor,uProgress, (vViewPosition.x+vViewPosition.y)*0.5 + 0.5);
+
+          vec4 matcapColor = mix(matcapColor2, matcapColor1, progress );
+        `,
+    );
+  }
 
   model.scene.traverse((child) => {
     if (child.name.includes("DOG")) {
@@ -59,6 +103,7 @@ function Dog() {
       child.material = branchMaterial;
     }
   });
+  dogMaterial.onBeforeCompile = onBeforeCompile;
   const dogModel = useRef(model);
 
   useGSAP(() => {
@@ -77,16 +122,118 @@ function Dog() {
       x: "+=0.1",
     })
       .to(dogModel.current.scene.rotation, {
-      x: `+=${Math.PI /15}`
-    })
-    .to(dogModel.current.scene.rotation, {
-      y: `-=${Math.PI}`
-    } ,"third")
-    .to(dogModel.current.scene.position, {
-      x: "-=0.5",
-      z:"+=0.4",
-      y:"-=0.02",
-    } ,"third")
+        x: `+=${Math.PI / 15}`,
+      })
+      .to(
+        dogModel.current.scene.rotation,
+        {
+          y: `-=${Math.PI}`,
+        },
+        "third",
+      )
+      .to(
+        dogModel.current.scene.position,
+        {
+          x: "-=0.5",
+          z: "+=0.4",
+          y: "-=0.02",
+        },
+        "third",
+      );
+  }, []);
+
+  useEffect(() => {
+    document.querySelector(`.title-item[img-title="tommorowland"]`).addEventListener("mouseenter", () => {
+      material.current.uMatcap1.value = mat19;
+      gsap.to(material.current.uProgress, {
+        value: 0.0,
+        duration: 0.3,
+        onComplete: () => {
+          material.current.uMatcap2.value = material.current.uMatcap1.value;
+          material.current.uProgress.value = 1.0;
+        },
+      });
+    
+    });
+    document.querySelector(`.title-item[img-title="navy-pier"]`).addEventListener("mouseenter", () => {
+      material.current.uMatcap1.value = mat8;
+      gsap.to(material.current.uProgress, {
+        value: 0.0,
+        duration: 0.3,
+        onComplete: () => {
+          material.current.uMatcap2.value = material.current.uMatcap1.value;
+          material.current.uProgress.value = 1.0;
+        },
+      });
+    });
+    document.querySelector(`.title-item[img-title="msi-chicago"]`).addEventListener("mouseenter", () => {
+      console.log("add .");
+      material.current.uMatcap1.value = mat9;
+      gsap.to(material.current.uProgress, {
+        value: 0.0,
+        duration: 0.3,
+        onComplete: () => {
+          material.current.uMatcap2.value = material.current.uMatcap1.value;
+          material.current.uProgress.value = 1.0;
+        },
+      });
+    });
+    document.querySelector(`.title-item[img-title="phone"]`).addEventListener("mouseenter", () => {
+      material.current.uMatcap1.value = mat12;
+      gsap.to(material.current.uProgress, {
+        value: 0.0,
+        duration: 0.3,
+        onComplete: () => {
+          material.current.uMatcap2.value = material.current.uMatcap1.value;
+          material.current.uProgress.value = 1.0;
+        },
+      });
+    });
+    document.querySelector(`.title-item[img-title="kikk"]`).addEventListener("mouseenter", () => {
+      material.current.uMatcap1.value = mat10;
+      gsap.to(material.current.uProgress, {
+        value: 0.0,
+        duration: 0.3,
+        onComplete: () => {
+          material.current.uMatcap2.value = material.current.uMatcap1.value;
+          material.current.uProgress.value = 1.0;
+        },
+      });
+    });
+    document.querySelector(`.title-item[img-title="kennedy"]`).addEventListener("mouseenter", () => {
+      material.current.uMatcap1.value = mat8;
+      gsap.to(material.current.uProgress, {
+        value: 0.0,
+        duration: 0.3,
+        onComplete: () => {
+          material.current.uMatcap2.value = material.current.uMatcap1.value;
+          material.current.uProgress.value = 1.0;
+        },
+      });
+    });
+    document.querySelector(`.title-item[img-title="opera"]`).addEventListener("mouseenter", () => {
+      material.current.uMatcap1.value = mat13;
+      gsap.to(material.current.uProgress, {
+        value: 0.0,
+        duration: 0.3,
+        onComplete: () => {
+          material.current.uMatcap2.value = material.current.uMatcap1.value;
+          material.current.uProgress.value = 1.0;
+        },
+      });
+    });
+    document.querySelector(`.titles`).addEventListener("mouseleave", () => {
+      material.current.uMatcap1.value = mat2;
+
+      gsap.to(material.current.uProgress, {
+        value: 0.0,
+        duration: 0.3,
+        onComplete: () => {
+          material.current.uMatcap2.value = material.current.uMatcap1.value;
+          material.current.uProgress.value = 1.0;
+        },
+      });
+    });
   }, []);
 
   return (
